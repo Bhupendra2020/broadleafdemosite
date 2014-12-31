57a86516aa3e3a0daf919325c9bd031956a84b3d
Bhupendra2020 <bhupendra.kumar@softcrylic.com>
2014-12-30 23:51:46 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+#!/bin/sh
+DIR=${DEPLOY_DIR:-/maven}
+echo "Checking *.war in $DIR"
+if [ -d $DIR ]; then
+  for i in $DIR/*.war; do
+     file=$(basename $i)
+     echo "Linking $i --> /tomcat/webapps/$file"
+     ln -s $i /tomcat/webapps/$file
+  done
+fi
+
 if [ ! -f /.tomcat_admin_created ]; then
     /create_tomcat_admin_user.sh
 fi
